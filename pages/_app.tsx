c58f60fc429aec0546beedb475c86a1fb4a757ae
Sameem <ssssameembaba@gmail.com>
2023-11-12 11:43:52 +0530

    bug fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,10 @@ import { ThirdwebProvider } from "@thirdweb-dev/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider clientId={process.env.NEXT_PUBLIC_THIRDWEB_API_KEY}>
+    <ThirdwebProvider clientId={"8e03e58bd47d918198dcfb69ce8457d6"}>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
 }
+
+// kkCIVHr7cjwzhf7SNX9XLqO2grrbiw_66tAB6yaFl6_hQQF9X8L0npjY0HrFx5yCDlVRGiIUodjOouRGZNn2dQ;
